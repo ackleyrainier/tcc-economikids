76decd8063fc36d3d29e1eb91576d58151028fc7
Ackley <ackley.lira@gmail.com>
2023-06-06 19:40:08 -0300

    criando projeto

---
+++ src/data/questions.js
@@ -0,0 +1,102 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Qual é o objetivo principal da poupança?",
+      answer: "Guardar dinheiro para emergências",
+      options: [
+        "Aumentar o poder de compra imediato",
+        "Guardar dinheiro para emergências",
+        " Obter ganhos de curto prazo",
+        "Financiar grandes compras",
+      ],
+    },
+    {
+      numb: 2,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Qual é o benefício chave da poupança em relação a guardar dinheiro em casa?",
+      answer: "Maior segurança contra roubo",
+      options: [
+        "Maior segurança contra roubo",
+        "Maior facilidade de acesso ao dinheiro",
+        "Maior potencial de retorno",
+        "Maior controle sobre os gastos",
+      ],
+    },
+    {
+      numb: 3,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Quais das seguintes opções são consideradas formas comuns de poupança?",
+      answer: "Conta poupança",
+      options: [
+        "Conta corrente",
+        "Conta poupança",
+        "Certificado de Depósito Bancário (CDB)",
+        "Ações de empresas",
+      ],
+    },
+    {
+      numb: 4,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "O que é o rendimento da poupança?",
+      answer: "Taxa de juros paga mensalmente sobre o saldo da poupança",
+      options: [
+        "Taxa de juros paga mensalmente sobre o saldo da poupança",
+        "Valor máximo que pode ser depositado em uma conta poupança",
+        "Taxa de juros cobrada pelos bancos para manter uma conta poupança",
+        "Valor mínimo necessário para abrir uma conta poupança",
+      ],
+    },
+    {
+      numb: 5,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Quais são as vantagens da poupança em relação a investimentos mais arriscados?",
+      answer: "Maior liquidez e acesso imediato aos fundos",
+      options: [
+        "Potencial de retorno mais alto",
+        "Maior liquidez e acesso imediato aos fundos",
+        "Menor necessidade de controle de gastos",
+        "Benefícios fiscais e isenções de impostos",
+      ],
+    },
+    {
+      numb: 6,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Qual é o limite de garantia do Fundo Garantidor de Créditos (FGC) para depósitos em poupança?",
+      answer: "R$ 250.000 por CPF e por instituição financeira",
+      options: [
+        "R$ 100.000 por CPF e por instituição financeira", 
+        "R$ 250.000 por CPF e por instituição financeira", 
+        "R$ 50.000 por CPF e por instituição financeira", 
+        "Não há limite de garantia para depósitos em poupança"
+      ],
+    },
+    {
+      numb: 7,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "O que é a data de aniversário da poupança?",
+      answer: "O dia em que é possível fazer saques e depósitos sem restrições",
+      options: [
+        "O dia em que a conta poupança foi aberta",
+        "O dia em que ocorre o pagamento dos juros", 
+        "O dia em que ocorre o débito mensal da taxa de administração", 
+        "O dia em que é possível fazer saques e depósitos sem restrições"
+      ],
+    },
+  ];
+  
